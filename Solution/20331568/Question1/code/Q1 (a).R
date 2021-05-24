62551ee3ba8235755659fc04d87784ba66beabbf
DeborahK <deborahkapenda1@gmail.com>
2021-05-24 20:38:16 +0200

    First commit

--- Solution/20331568/Question1/code/Q1 (a).R
+++ Solution/20331568/Question1/code/Q1 (a).R
@@ -1,12 +1,87 @@
 # Write your function here
 
-func <- function(x,y){
+q1 <- function () {
 
-    print("plese please")
+library(readr)
+df <- read_csv("D:/20331568/Solution/20331568/Question1/data/Movies.csv")
 
-    Res = x * y
-
-    return(Res)
+df <- rename_with(df, ~ tolower(gsub("%", ".", .x, fixed = TRUE)))
+df <- rename_with(df, ~ tolower(gsub(".", "", .x, fixed = TRUE)))
+df <- rename_with(df, ~ tolower(gsub(" ", "", .x, fixed = TRUE)))
+return(df)
 
 }
+
+
+
+r <- function (d) {
+
+    p1 <- df %>% group_by(film) %>% filter(rottentomatoes >= 80)
+
+    d <- ggplot(p1, aes(x = genre, y= rottentomatoes, colour = genre))+ geom_point() + labs(
+        x = "Film",
+        y = "Rotten tomatoes",
+        title = "Movie Ratings",
+        fill ="#f68060"
+    ) + theme_bw()
+
+
+    return(d)
+
+}
+
+
+rr <- function (e) {
+
+    p1 <- df %>% group_by(film) %>% filter(audiencescore >= 85)
+
+    e <- ggplot(p1, aes(x = genre, y= rottentomatoes, colour = genre))+ geom_point() + labs(
+        x = "Film",
+        y = "Rotten tomatoes",
+        title = "Movie Ratings",
+        fill ="#f68060"
+    ) + theme_bw()
+
+
+    return(e)
+
+}
+
+
+
+dis <- function (b) {
+
+    p1 <- df %>% group_by(leadstudio) %>% filter(profitability >= 2)
+
+    b <- ggplot(p1, aes(x = genre, y= profitability, colour = leadstudio))+ geom_point() + labs(
+        x = "genre",
+        y = "profitability",
+        title = "Movie Ratings by Lead Studios",
+        fill ="#f68060"
+    ) + theme_bw()
+
+
+    return(b)
+
+}
+
+
+h <- function (t) {
+
+    p1 <- df %>% group_by(film) %>% filter( worldwidegross >= 100)
+
+    t <- ggplot(p1, aes(x = genre, y= worldwidegross, colour = genre))+ geom_point() + labs(
+        x = "genre",
+        y = "worldwidegross",
+        title = "Movie Ratings by Worldwide gross",
+        fill ="#f68060"
+    ) + theme_bw()
+
+
+    return(t)
+
+}
+
+
+
 
--- Solution/20331568/Question2/code/Q2 (a).R
+++ Solution/20331568/Question2/code/Q2 (a).R
@@ -1,11 +1,66 @@
+##Import the data
 
-yes <- function(x,y){
+data1 <- function () {
+if(!require(dplyr))install.packages("dplyr")
+library(dplyr)
+if(!require(dplyr))install.packages("dplyr")
+library(dplyr)
 
-    print("plese please")
+# Using the file location, import all csv files
+tbl <-  list.files("D:/20331568/Solution/20331568/Question2/data/Forbes"  ,
+                   pattern = "*.csv",
+                   full.names = T) %>%
+    map_df(~read_csv(., col_types = cols(.default = "c")))
 
-    Res = x / y
 
-    return(Res)
+tbl$NetWorth<- gsub("[a-zA-Z | $]","",tbl$NetWorth)
+
+tbl  <- tbl %>% mutate(NetWorth = as.numeric(NetWorth), Rank  = as.numeric(Rank),
+                       Age = as.numeric(Age),
+                       Name = as.factor(Name),
+                       Industry = as.factor(Industry),
+                       Source = as.factor(Source),
+                       Country = as.factor(Country))
+return(tbl)
 
 }
+
+
+figure1 <- function (g) {
+
+plot <- tbl %>% group_by(Country) %>% filter(Age <=30)
+
+g <- ggplot(plot, aes(x = Country, y= NetWorth, colour = Country))+ geom_point() + labs(
+    x = "Country",
+    y = "NetWorth",
+    title = "Billionaires under the Age of 30",
+    fill ="#f68060"
+) + theme_bw()
+
+return(g)
+
+}
+
+
+
+
+figure2 <- function (d) {
+
+    plot <- tbl %>% group_by(Country) %>% filter(Rank <= 10 )
+
+    d <- ggplot(plot, aes(x = Country, y= NetWorth, colour = Country))+ geom_point() + labs(
+        x = "Country",
+        y = "Rank",
+        title = "Top 10 Billionaires",
+        fill ="#f68060"
+    ) + theme_bw()
+
+    return(d)
+
+}
+
+
+
+
+
 

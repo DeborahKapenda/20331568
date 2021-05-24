3832babe6e0455278ae0de4a4a0a4c7038136a2b
DeborahK <deborahkapenda1@gmail.com>
2021-05-24 15:04:25 +0200

    First commit

---
+++ Solution/20331568/Question1/code/Q1 (a).R
@@ -0,0 +1,12 @@
+# Write your function here
+
+func <- function(x,y){
+
+    print("plese please")
+
+    Res = x * y
+
+    return(Res)
+
+}
+
---
+++ Solution/20331568/Question2/code/Q2 (a).R
@@ -0,0 +1,11 @@
+
+yes <- function(x,y){
+
+    print("plese please")
+
+    Res = x / y
+
+    return(Res)
+
+}
+
